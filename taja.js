eaff4ec4cf673cb823e81404f192a300ca5f96e0
alisilsan <166475772+alisilsan@users.noreply.github.com>
2024-06-03 14:53:23 +0900

    폴더 추가

--- taja.js
+++ taja.js
@@ -14,26 +14,26 @@ function startTest() {
 }
 
 const questions = [
-    { image: "./img/taja_1.png", answer: "스스로 불러온 재앙" },
-    { image: "./img/taja_2.png", answer: "별걸 다 줄이네" },
-    { image: "./img/taja_3.png", answer: "중요한건 꺾이지않는 마음" },
-    { image: "./img/taja_4.png", answer: "옥상에서 떨어진 메주" },
-    { image: "./img/taja_5.png", answer: "시선 강탈" },
-    { image: "./img/taja_6.png", answer: "마음의 상처" },
-    { image: "./img/taja_7.png", answer: "꾸민듯 안 꾸민듯" },
-    { image: "./img/taja_8.png", answer: "점심 메뉴 추천" },
-    { image: "./img/taja_9.png", answer: "Too Much Information" },
-    { image: "./img/taja_10.png", answer: "억지 텐션" },
-    { image: "./img/taja_11.png", answer: "자존심 강한 두 천재의 대결" },
-    { image: "./img/taja_12.png", answer: "불보고 멍때리기" },
-    { image: "./img/taja_13.png", answer: "얼어 죽어도 아이스아메리카노" },
-    { image: "./img/taja_14.png", answer: "자연스러운 만남 추구" },
-    { image: "./img/taja_15.png", answer: "소소하지만 확실한 행복" },
-    { image: "./img/taja_16.png", answer: "엄격 근엄 진지" },
-    { image: "./img/taja_17.png", answer: "무엇이든 물어보세요" },
-    { image: "./img/taja_18.png", answer: "갑자기 분위기 싸해짐" },
-    { image: "./img/taja_19.png", answer: "아이스 바닐라 라떼" },
-    { image: "./img/taja_20.png", answer: "구독 취소" }
+    { image: "./img/taja/taja_1.png", answer: "스스로 불러온 재앙" },
+    { image: "./img/taja/taja_2.png", answer: "별걸 다 줄이네" },
+    { image: "./img/taja/taja_3.png", answer: "중요한건 꺾이지않는 마음" },
+    { image: "./img/taja/taja_4.png", answer: "옥상에서 떨어진 메주" },
+    { image: "./img/taja/taja_5.png", answer: "시선 강탈" },
+    { image: "./img/taja/taja_6.png", answer: "마음의 상처" },
+    { image: "./img/taja/taja_7.png", answer: "꾸민듯 안 꾸민듯" },
+    { image: "./img/taja/taja_8.png", answer: "점심 메뉴 추천" },
+    { image: "./img/taja/taja_9.png", answer: "Too Much Information" },
+    { image: "./img/taja/taja_10.png", answer: "억지 텐션" },
+    { image: "./img/taja/taja_11.png", answer: "자존심 강한 두 천재의 대결" },
+    { image: "./img/taja/taja_12.png", answer: "불보고 멍때리기" },
+    { image: "./img/taja/taja_13.png", answer: "얼어 죽어도 아이스아메리카노" },
+    { image: "./img/taja/taja_14.png", answer: "자연스러운 만남 추구" },
+    { image: "./img/taja/taja_15.png", answer: "소소하지만 확실한 행복" },
+    { image: "./img/taja/taja_16.png", answer: "엄격 근엄 진지" },
+    { image: "./img/taja/taja_17.png", answer: "무엇이든 물어보세요" },
+    { image: "./img/taja/taja_18.png", answer: "갑자기 분위기 싸해짐" },
+    { image: "./img/taja/taja_19.png", answer: "아이스 바닐라 라떼" },
+    { image: "./img/taja/taja_20.png", answer: "구독 취소" }
     // ...총 20개의 문제6 여기 추가
     
 ];
