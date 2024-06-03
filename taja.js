35cff22f6dbd1b5ca01694a5f0ebb284ebac852e
alisilsan <166475772+alisilsan@users.noreply.github.com>
2024-06-03 14:43:18 +0900

    taja img 추가

--- taja.js
+++ taja.js
@@ -17,24 +17,24 @@ const questions = [
     { image: "./img/taja_1.png", answer: "스스로 불러온 재앙" },
     { image: "./img/taja_2.png", answer: "별걸 다 줄이네" },
     { image: "./img/taja_3.png", answer: "중요한건 꺾이지않는 마음" },
-    { image: "images/image4.jpg", answer: "answer4" },
-    { image: "images/image5.jpg", answer: "answer5" },
-    { image: "images/image6.jpg", answer: "answer6" },
-    { image: "images/image7.jpg", answer: "answer7" },
-    { image: "images/image8.jpg", answer: "answer8" },
-    { image: "images/image9.jpg", answer: "answer9" },
-    { image: "images/image10.jpg", answer: "answer10" },
-    { image: "images/image11.jpg", answer: "answer11" },
-    { image: "images/image12.jpg", answer: "answer12" },
-    { image: "images/image13.jpg", answer: "answer13" },
-    { image: "images/image14.jpg", answer: "answer14" },
-    { image: "images/image15.jpg", answer: "answer15" },
-    { image: "images/image16.jpg", answer: "answer16" },
-    { image: "images/image17.jpg", answer: "answer17" },
-    { image: "images/image18.jpg", answer: "answer18" },
-    { image: "images/image19.jpg", answer: "answer19" },
-    { image: "images/image20.jpg", answer: "answer20" }
-    // ...총 20개의 문제를 여기 추가
+    { image: "./img/taja_4.png", answer: "옥상에서 떨어진 메주" },
+    { image: "./img/taja_5.png", answer: "시선 강탈" },
+    { image: "./img/taja_6.png", answer: "마음의 상처" },
+    { image: "./img/taja_7.png", answer: "꾸민듯 안 꾸민듯" },
+    { image: "./img/taja_8.png", answer: "점심 메뉴 추천" },
+    { image: "./img/taja_9.png", answer: "Too Much Information" },
+    { image: "./img/taja_10.png", answer: "억지 텐션" },
+    { image: "./img/taja_11.png", answer: "자존심 강한 두 천재의 대결" },
+    { image: "./img/taja_12.png", answer: "불보고 멍때리기" },
+    { image: "./img/taja_13.png", answer: "얼어 죽어도 아이스아메리카노" },
+    { image: "./img/taja_14.png", answer: "자연스러운 만남 추구" },
+    { image: "./img/taja_15.png", answer: "소소하지만 확실한 행복" },
+    { image: "./img/taja_16.png", answer: "엄격 근엄 진지" },
+    { image: "./img/taja_17.png", answer: "무엇이든 물어보세요" },
+    { image: "./img/taja_18.png", answer: "갑자기 분위기 싸해짐" },
+    { image: "./img/taja_19.png", answer: "아이스 바닐라 라떼" },
+    { image: "./img/taja_20.png", answer: "구독 취소" }
+    // ...총 20개의 문제6 여기 추가
     
 ];
 
@@ -74,13 +74,13 @@ function showFinalResult() {
 
     let resultMessage = '';
     if (correctCount >= 12) {
-        resultMessage = 'Excellent! You scored between 12 and 20.';
+        resultMessage = '대단해요! 혹시 MZ 신가요?';
     } else if (correctCount >= 8) {
-        resultMessage = 'Good job! You scored between 8 and 11.';
+        resultMessage = '좋아요! 유행에는 따라가는거 같네요~';
     } else if (correctCount >= 4) {
-        resultMessage = 'Not bad! You scored between 4 and 7.';
+        resultMessage = '흠... 조금 유행에 뒤쳐지시는거 같은데요?';
     } else {
-        resultMessage = 'Keep practicing! You scored between 0 and 3.';
+        resultMessage = '킁킁.. 어디서 홍삼캔디 냄새 나지 않아요?';
     }
     document.getElementById('result-message').textContent = resultMessage;
 }
