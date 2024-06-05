75afe863d9eb0b9dfb343a5cbd6533bda7ecc27d
alisilsan <166475772+alisilsan@users.noreply.github.com>
2024-06-05 13:06:43 +0900

    .

--- fortune.js
+++ fortune.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     // 포춘 쿠키 이미지 클릭 이벤트
     $('#fortune-cookie').click(function() {
         $(this).fadeOut(800); // 기존 포춘 쿠키 이미지 사라짐
-        $('.infocontainer').fadeIn(3000) // infocontainer 컨테이너 출력
+        $('.infocontainer').fadeIn(3000); // infocontainer 컨테이너 출력
     });
 
     // fortune-info 클릭 이벤트
@@ -11,29 +11,30 @@ $(document).ready(function() {
         $('.fortune-info').fadeOut();
         
         setTimeout(function() {
-        $('<div class="fortuneSecondimage"><img src="./img/fortunecookieBreak.png" alt="Temporary Fortune Cookie"></div>').insertAfter('.container').hide().fadeIn(1000);
-        console.log("123") // 임시 포춘쿠키 이미지 출력
+            $('<div class="fortuneSecondimage"><img src="./img/fortunecookieBreak.png" alt="Temporary Fortune Cookie"></div>').insertAfter('.container').hide().fadeIn(1000);
+            console.log("123"); // 임시 포춘쿠키 이미지 출력
         }, 1000);
 
-        
         setTimeout(function() {
             $('.fortuneSecondimage').fadeOut(500); // 임시 포춘쿠키 이미지 사라짐
-        // 랜덤으로 운세 쪽지 출력
+            // 랜덤으로 운세 쪽지 출력
             var fortunes = ['./img/text1.png', './img/text2.png', './img/text3.png', './img/text4.png', './img/text5.png'];
             var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
             $('<img src="' + randomFortune + '" alt="Fortune" class="randomFortune">').appendTo('body').fadeOut(0).fadeIn(3000); // randomfortune 클래스명 추가
             setTimeout(function() {
-            $('<button class="restart-button">다시 시작</button>').appendTo('body'); // 처음으로 돌아가는 버튼 출력
-            // 처음으로 돌아가는 버튼 클릭 이벤트
-            $('.restart-button').click(function() {
-            location.reload(); // 페이지 새로고침
-            });
-            // $('<button class="reset-button">메인으로 이동</button>').appendTo('body'); // 처음으로 돌아가는 버튼 출력
-            // $('.reset-button').click(function() {
-            // location.window,href= "./index.html";
-            // });
+                $('<button class="restart-button">다시 시작</button>').appendTo('body'); // 처음으로 돌아가는 버튼 출력
+                $('<button class="reset-button">홈으로</button>').appendTo('body'); // 홈으로 이동하는 버튼 출력
+
+                // 처음으로 돌아가는 버튼 클릭 이벤트
+                $('.restart-button').click(function() {
+                    location.reload(); // 페이지 새로고침
+                });
+
+                // 홈으로 이동하는 버튼 클릭 이벤트
+                $('.reset-button').click(function() {
+                    window.location.href = 'index.html'; // index.html로 이동
+                });
             }, 5000);
         }, 3000);
-        
     });
 });
